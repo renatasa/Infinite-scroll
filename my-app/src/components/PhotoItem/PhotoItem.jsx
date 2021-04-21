cf23b5e544973cab6c770ddb7ea73fe2f642e972
Begemotas <cheminiselementas@gmail.com>
2021-04-21 23:35:59 +0300

    infitite scroll added

--- my-app/src/components/PhotoItem/PhotoItem.jsx
+++ my-app/src/components/PhotoItem/PhotoItem.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 const photoItem = (props) => {
   const photoSrc = `https://live.staticflickr.com/${props.server}/${props.id}_${props.secret}_w.jpg`;
-  const photo = (
-    <img src={photoSrc} alt={props.title} width="300" height="200"></img>
-  );
-  return <div>{photo}</div>;
+//   const photo = (
+//     <img className='photoItem' src={photoSrc} alt={props.title} width="300" height="200"></img>
+//   );
+//   return <div className='photoItem'>{photo}</div>;
+
+return <img className='photoItem' src={photoSrc} alt={props.title} width="300" height="200"></img>
 };
 
 export default photoItem;
--- my-app/src/containers/PhotoStream/PhotoStream.jsx
+++ my-app/src/containers/PhotoStream/PhotoStream.jsx
@@ -4,19 +4,27 @@ import PhotoItem from "../../components/PhotoItem/PhotoItem";
 export class PhotoStream extends Component {
   state = {
     photoAttributesArray: [],
+    currentPage:0,
     pages: 10,
     photosPerPage: 10,
+    scrolling: false
   };
+
   componentDidMount() {
+    this.loadPhotos();
+    this.scrollListener = window.addEventListener('scroll', (e)=>{
+      this.handleScroll()
+    })
+  }
+
+  loadPhotos = () => {
     let req = new XMLHttpRequest();
     req.onreadystatechange = () => {
       if (req.readyState == XMLHttpRequest.DONE) {
         if (req.status == 200) {
-          console.log("good request");
           let parser = new DOMParser();
           let xml = parser.parseFromString(req.response, "application/xml");
           let xmlPhotoElements = xml.getElementsByTagName("photo");
-          console.log(xml);
 
           let photoAttributesArr = [];
           for (let i = 0; i < xmlPhotoElements.length; i++) {
@@ -27,32 +35,58 @@ export class PhotoStream extends Component {
             photoObj.title = xmlPhotoElements[i].getAttribute("title");
             photoAttributesArr.push(photoObj);
           }
-          this.setState({ photoAttributesArray: photoAttributesArr });
+          this.setState({
+            photoAttributesArray: [
+              ...this.state.photoAttributesArray,
+              ...photoAttributesArr,
+            ],
+            scrolling:false
+          });
         }
       }
     };
 
-    let url = `https://www.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=${process.env.REACT_APP_KEY}&per_page=${this.state.photosPerPage}&page=${this.state.pages}&format=rest`;
+    let url = `https://www.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=${process.env.REACT_APP_KEY}&per_page=${this.state.photosPerPage}&page=${this.state.currentPage}&format=rest`;
     req.open("GET", url, true);
     req.send();
+  };
+
+  loadMore=()=>{
+    this.setState(prevState=>({
+      page: prevState.currentPage + 1,
+      scrolling: true
+    }), this.loadPhotos)
+  }
+
+  handleScroll=()=>{
+    const {scrolling, pages, currentPage} = this.state
+    if(scrolling) return
+    if(pages<=currentPage) return
+    const lastPhoto=document.querySelector('div.photos > img:last-child')
+    const lastPhotoOffset=lastPhoto.offsetTop + lastPhoto.clientHeight
+    const pageOffset=window.pageYOffset + window.innerHeight
+    const bottomOffset=20
+    if(pageOffset > lastPhotoOffset-bottomOffset) this.loadMore()   
   }
 
   createPhotoStream = () => {
     if (this.state.photoAttributesArray.length > 0) {
-      return this.state.photoAttributesArray.map((photo) => (
+      return this.state.photoAttributesArray.map((photo, key) => (
         <PhotoItem
           server={photo.server}
           secret={photo.secret}
           id={photo.id}
           title={photo.title}
-          key={photo.id}
+          key={key}
         />
       ));
     } else return [];
   };
 
   render() {
-    return <div>{this.createPhotoStream()}</div>;
+    return <div>
+      <div className="photos"> {this.createPhotoStream()} </div>
+    </div>;
   }
 }
 
