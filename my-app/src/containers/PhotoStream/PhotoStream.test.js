15cd5dc87bbd932e9370f7371048ad80a6d1882e
Begemotas <cheminiselementas@gmail.com>
2021-06-05 15:53:51 +0300

    errorservice.js removed

--- my-app/src/containers/PhotoStream/PhotoStream.test.js
+++ my-app/src/containers/PhotoStream/PhotoStream.test.js
@@ -10,19 +10,21 @@ Enzyme.configure({ adapter: new Adapter() });
 
 beforeEach(() => {
   const responseString =
-'jsonFlickrApi({"photos":{"page":1,"pages":200,"perpage":5,"total":1000,"photo":[{"id":"someId0","owner":"someOwner0","secret":"someSecret0","server":"someServer0","farm":66,"title":"someTitle0","ispublic":1,"isfriend":0,"isfamily":0},{"id":"someId1","owner":"someOwner1","secret":"someSecret1","server":"someServer1","farm":66,"title":"someTitle1","ispublic":1,"isfriend":0,"isfamily":0},{"id":"someId2","owner":"someOwner2","secret":"someSecret2","server":"someServer2","farm":66,"title":"someTitle2","ispublic":1,"isfriend":0,"isfamily":0},{"id":"someId3","owner":"someOwner3","secret":"someSecret3","server":"someServer3","farm":66,"title":"someTitle3","ispublic":1,"isfriend":0,"isfamily":0},{"id":"someId4","owner":"someOwner4","secret":"someSecret4","server":"someServer4","farm":66,"title":"someTitle4","ispublic":1,"isfriend":0,"isfamily":0}]},"stat":"ok"})';
+    'jsonFlickrApi({"photos":{"page":1,"pages":200,"perpage":5,"total":1000,"photo":[{"id":"someId0","owner":"someOwner0","secret":"someSecret0","server":"someServer0","farm":66,"title":"someTitle0","ispublic":1,"isfriend":0,"isfamily":0},{"id":"someId1","owner":"someOwner1","secret":"someSecret1","server":"someServer1","farm":66,"title":"someTitle1","ispublic":1,"isfriend":0,"isfamily":0},{"id":"someId2","owner":"someOwner2","secret":"someSecret2","server":"someServer2","farm":66,"title":"someTitle2","ispublic":1,"isfriend":0,"isfamily":0},{"id":"someId3","owner":"someOwner3","secret":"someSecret3","server":"someServer3","farm":66,"title":"someTitle3","ispublic":1,"isfriend":0,"isfamily":0},{"id":"someId4","owner":"someOwner4","secret":"someSecret4","server":"someServer4","farm":66,"title":"someTitle4","ispublic":1,"isfriend":0,"isfamily":0}]},"stat":"ok"})';
 
-return fetch.mockReturnValue(Promise.resolve(new Response(responseString)));
+  return fetch.mockReturnValue(Promise.resolve(new Response(responseString)));
 });
 
-const createPhotoStreamComponent=(expectedState)=>{
+const createPhotoStreamComponent = (expectedState) => {
   const wrapper = shallow(<PhotoStream />);
-  return expectedState ? wrapper.setState(expectedState) : findByTestAttr(wrapper, "component-PhotoStream")
-}
+  return expectedState
+    ? wrapper.setState(expectedState)
+    : findByTestAttr(wrapper, "component-PhotoStream");
+};
 
 test("When app is loaded without errors, then PhotoStream is shown", () => {
   // act
-  const photoStream = createPhotoStreamComponent()
+  const photoStream = createPhotoStreamComponent();
 
   // assert
   expect(photoStream.length).toBe(1);
@@ -30,15 +32,18 @@ test("When app is loaded without errors, then PhotoStream is shown", () => {
 
 test("When app is loading, then Spinner is shown instead of PhotoStream", () => {
   // arrange
-  const expectedState={
+  const expectedState = {
     error: "",
     loading: true,
-  }
+  };
 
   // act
-  const wrapperPhotoStream = createPhotoStreamComponent(expectedState)
+  const wrapperPhotoStream = createPhotoStreamComponent(expectedState);
   const spinner = findByTestAttr(wrapperPhotoStream, "component-Spinner");
-  const error= findByTestAttr(wrapperPhotoStream, "component-PhotoStreamError")
+  const error = findByTestAttr(
+    wrapperPhotoStream,
+    "component-PhotoStreamError"
+  );
 
   // assert
   expect(spinner.length).toBe(1);
@@ -47,15 +52,18 @@ test("When app is loading, then Spinner is shown instead of PhotoStream", () => 
 
 test("When app receives error message from api, then error is shown instead of PhotoStream", () => {
   // arrange
-  const expectedState={
+  const expectedState = {
     error: "this is error",
     loading: false,
-  }
+  };
 
   // act
-  const wrapperPhotoStream = createPhotoStreamComponent(expectedState)
+  const wrapperPhotoStream = createPhotoStreamComponent(expectedState);
   const spinner = findByTestAttr(wrapperPhotoStream, "component-Spinner");
-  const error= findByTestAttr(wrapperPhotoStream, "component-PhotoStreamError")
+  const error = findByTestAttr(
+    wrapperPhotoStream,
+    "component-PhotoStreamError"
+  );
 
   // assert
   expect(spinner.length).toBe(0);
--- my-app/src/containers/services/errorService.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
-
-export const createErrorMessage = (error, loading) => {
-  if (error.length > 0 && !loading) {
-    return <ErrorMessage error={error} errorType={"errorFetchingPhotos"} />;
-  } else return null;
-};
